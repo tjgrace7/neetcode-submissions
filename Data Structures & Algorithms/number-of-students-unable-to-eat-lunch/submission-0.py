cbccec802912c0541dbfce45c439f93321776bd6
tjgrace7 <121319122+tjgrace7@users.noreply.github.com>
2026-09-25 09:39:53 -0500

    Add: number-of-students-unable-to-eat-lunch - submission-0

---
+++ Data Structures & Algorithms/number-of-students-unable-to-eat-lunch/submission-0.py
@@ -0,0 +1,19 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class Solution:
+    def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
+        circ = students.count(0)
+        square = len(students)-circ
+        for s in sandwiches:
+            if s == 0:
+                if circ == 0:
+                    return square
+                circ -= 1
+            else:
+                if square == 0:
+                    return circ
+                square -= 1
+        return 0
